cf0970cd00ad7dae67b108f17496dedb083bdacd
Dida En <den.elfsuju@gmail.com>
2022-01-20 14:01:37 +0700

    Kita akan menerapkan fungsi Object.create() pada Function Declaration.

--- script.js
+++ script.js
@@ -30,28 +30,28 @@
 // Keuntungannya kalian tidak perlu membuat duplikat dari objectnya
 // Nanti kalo mau bikin objectnya 2 atau 3 tinggan instansiasinya yang berulangkali
 
-// Ini kita membuat BluE Print nya
-// function Mahasiswa(nama, energi) {
-//     // Awalnya sama yaitu dengan membuat object KOSONGAN
-//     let mahasiswa = {};
-//     mahasiswa.nama = nama;
-//     mahasiswa.energi = energi;
+// Ini kita membuat Blue Print nya
+function Mahasiswa(nama, energi) {
+    // Awalnya sama yaitu dengan membuat object KOSONGAN
+    let mahasiswa = {};
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
 
-//     // Ini juga mirip tapi ini bentuknya jadi seperti class
-//     // Pake . (dot)
-//     mahasiswa.makan = function (porsi) {
-//         this.energi += porsi;
-//         console.log(`Selamat makan, ${this.nama}. Energimu sekarang menjadi ${this.energi}.`);
-//     }
+    // Ini juga mirip tapi ini bentuknya jadi seperti class
+    // Pake . (dot)
+    mahasiswa.makan = function (porsi) {
+        this.energi += porsi;
+        console.log(`Selamat makan, ${this.nama}. Energimu sekarang menjadi ${this.energi}.`);
+    }
 
-//     // Membuat method main yang akan mengurangi energi
-//     mahasiswa.main = function (jam) {
-//         this.energi -= jam;
-//         console.log(`Hallo ${this.nama}, selamat bermain! Energimu sekarang tinggal ${this.energi}.`);
-//     }
+    // Membuat method main yang akan mengurangi energi
+    mahasiswa.main = function (jam) {
+        this.energi -= jam;
+        console.log(`Hallo ${this.nama}, selamat bermain! Energimu sekarang tinggal ${this.energi}.`);
+    }
 
-//     return mahasiswa;
-// }
+    return mahasiswa;
+}
 
 // // INISIASI OBJECT
 // let dida = Mahasiswa("Dida", 10);
@@ -62,30 +62,30 @@
 
 
 // Constructor Function
-function Mahasiswa(nama, energi) {
-    // Pada constructor function, kita tidak memerlukan deklarasi array kosong
-    // let mahasiswa = {};
+// function Mahasiswa(nama, energi) {
+//     // Pada constructor function, kita tidak memerlukan deklarasi array kosong
+//     // let mahasiswa = {};
 
-    // Selain itu mahasiswa ini diganti menjadi this
-    this.nama = nama;
-    this.energi = energi;
+//     // Selain itu mahasiswa ini diganti menjadi this
+//     this.nama = nama;
+//     this.energi = energi;
 
-    // Ini juga mirip tapi ini bentuknya jadi seperti class
-    // Pake . (dot)
-    this.makan = function (porsi) {
-        this.energi += porsi;
-        console.log(`Selamat makan, ${this.nama}. Energimu sekarang menjadi ${this.energi}.`);
-    }
+//     // Ini juga mirip tapi ini bentuknya jadi seperti class
+//     // Pake . (dot)
+//     this.makan = function (porsi) {
+//         this.energi += porsi;
+//         console.log(`Selamat makan, ${this.nama}. Energimu sekarang menjadi ${this.energi}.`);
+//     }
 
-    // Membuat method main yang akan mengurangi energi
-    this.main = function (jam) {
-        this.energi -= jam;
-        console.log(`Hallo ${this.nama}, selamat bermain! Energimu sekarang tinggal ${this.energi}.`);
-    }
+//     // Membuat method main yang akan mengurangi energi
+//     this.main = function (jam) {
+//         this.energi -= jam;
+//         console.log(`Hallo ${this.nama}, selamat bermain! Energimu sekarang tinggal ${this.energi}.`);
+//     }
 
-    // Pada Constructor Function ini kita juga tidak memerlukan return
-    // return mahasiswa;
-}
+// Pada Constructor Function ini kita juga tidak memerlukan return
+// return mahasiswa;
+// }
 
 // Untuk inisiasi, seperti inisiasi pada Function Declaration
 // Namun untuk Constructor Function ini perlu menambahkan keyword di depan
